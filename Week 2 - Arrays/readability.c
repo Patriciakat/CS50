e13e4ffbc843bffc2836a87dbf21bf9c449ae811
Patriciakat <patricija.katinaite@gmail.com>
2023-11-19 07:10:43 +0200

    Week 2

---
+++ Week 2 - Arrays/readability.c
@@ -0,0 +1,86 @@
+﻿// a program that calculates the approximate grade level needed to comprehend some text
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    string text = get_string("Text:\n");
+
+    int words = count_words(text);
+
+    double L = (double) count_letters(text) / (double) words * 100;
+    double S = (double) count_sentences(text) / (double) words * 100;
+
+    double index = 0.0588 * L - 0.296 * S - 15.8;
+
+    if (index < 1) {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16) {
+        printf("Grade 16+\n");
+    }
+    else {
+        printf("Grade: %0.f\n", index);
+    }
+}
+
+int count_letters(string text) {
+
+    int letters = 0;
+    int current;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        current = text[i];
+
+        if (isalpha(current))
+        {
+            letters++;
+        }
+    }
+
+    return letters;
+}
+
+int count_words(string text) {
+
+    int words = 1;
+    int current;
+
+    for (int i = 0; i <= strlen(text); i++)
+    {
+        current = text[i];
+
+        if (current == 32)
+        {
+            words++;
+        }
+    }
+
+    return words;
+}
+
+int count_sentences(string text) {
+
+    int sentence = 0;
+    int current;
+
+    for (int i = 0; i <= strlen(text); i++)
+    {
+        current = text[i];
+
+        if (current == 46 || current == 33 || current == 63)
+        {
+            sentence++;
+        }
+    }
+
+    return sentence;
+}
---
+++ Week 2 - Arrays/wordle/wordle.c
@@ -0,0 +1,157 @@
+﻿//a program that behaves similarly to the popular Wordle daily word game
+
+#include <cs50.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define LISTSIZE 1000
+
+// values for colors and score (EXACT == right letter, right place; CLOSE == right letter, wrong place; WRONG == wrong letter)
+#define EXACT 2
+#define CLOSE 1
+#define WRONG 0
+
+#define GREEN   "\e[38;2;255;255;255;1m\e[48;2;106;170;100;1m"
+#define YELLOW  "\e[38;2;255;255;255;1m\e[48;2;201;180;88;1m"
+#define RED     "\e[38;2;255;255;255;1m\e[48;2;220;20;60;1m"
+#define RESET   "\e[0;39m"
+
+string get_guess(int wordsize);
+int check_word(string guess, int wordsize, int status[], string choice);
+void print_word(string guess, int wordsize, int status[]);
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./wordle wordsize\n");
+        return 1;
+    }
+
+    int wordsize = 0;
+
+    if (argv[1][0] == 5 + '0' || argv[1][0] == 6 + '0' || argv[1][0] == 7 + '0' || argv[1][0] == 8 + '0')
+    {
+        wordsize = argv[1][0] - '0';
+        printf("wordsize = %c\n", wordsize);
+    }
+    else
+    {
+        printf("Error: wordsize must be either 5, 6, 7, or 8\n");
+        return 1;
+    }
+
+    char wl_filename[6];
+    sprintf(wl_filename, "%i.txt", wordsize);
+    FILE *wordlist = fopen(wl_filename, "r");
+    if (wordlist == NULL)
+    {
+        printf("Error opening file %s.\n", wl_filename);
+        return 1;
+    }
+
+    char options[LISTSIZE][wordsize + 1];
+
+    for (int i = 0; i < LISTSIZE; i++)
+    {
+        fscanf(wordlist, "%s", options[i]);
+    }
+
+    srand(time(NULL));
+    string choice = options[rand() % LISTSIZE];
+
+    int guesses = wordsize + 1;
+    bool won = false;
+
+    printf(GREEN"This is WORDLE50"RESET"\n");
+    printf("You have %i tries to guess the %i-letter word I'm thinking of\n", guesses, wordsize);
+
+    for (int i = 0; i < guesses; i++)
+    {
+        string guess = get_guess(wordsize);
+
+        int status[wordsize];
+
+        for (int j = 0; j < wordsize; j++)
+        {
+            status[j] = WRONG;
+        }
+
+        int score = check_word(guess, wordsize, status, choice);
+
+        printf("Guess %i: ", i + 1);
+
+        print_word(guess, wordsize, status);
+
+        if (score == EXACT * wordsize)
+        {
+            won = true;
+            break;
+        }
+    }
+
+    if (won)
+        printf("You won!\n");
+    else
+        printf("The word was: %s\n", choice);
+
+    return 0;
+}
+
+string get_guess(int wordsize)
+{
+    string guess;
+
+    do
+    {
+        guess = get_string("Input a 5-letter word: \n");
+    } while (strlen(guess) != wordsize);
+
+    return guess;
+}
+
+int check_word(string guess, int wordsize, int status[], string choice)
+{
+    int score = 0;
+    printf("choice = %s\n", choice);
+
+    for (int i = 0; i < wordsize; i++) {
+
+        for (int j = 0; j < wordsize; j++)
+        {
+
+            if (guess[i] == choice[j] && i == j)
+            {
+                status[i] = EXACT;
+                score = score + EXACT;
+                break;
+            }
+            else if (guess[i] == choice[j])
+            {
+                status[i] = CLOSE;
+            }
+        }
+    }
+    return score;
+}
+
+void print_word(string guess, int wordsize, int status[])
+{
+    // print word character-for-character with correct color coding, then reset terminal font to normal
+    for (int i = 0; i < wordsize; i++)
+    {
+            if (status[i] == 2)
+                printf(GREEN"%c"RESET, guess[i]);
+
+            else if (status[i] == 1)
+                printf(YELLOW"%c"RESET, guess[i]);
+
+            else
+                printf(RED"%c"RESET, guess[i]);
+    }
+    printf("\n");
+
+    return;
+}
