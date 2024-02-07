9ad6f8483af032a8d2343b63d6639562db63f7e3
Patricija Katinaite <patricija.katinaite@gmail.com>
2024-02-08 01:06:39 +0200

    Week 4

---
+++ Week 4 - Memory/license.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./read infile\n");
+        return 1;
+    }
+
+    // Create buffer to read into
+    char buffer[7];
+
+    // Create array to store plate numbers
+    char *plates[8];
+
+    FILE *infile = fopen(argv[1], "r");
+
+    int idx = 0;
+
+    while (fread(buffer, 1, 7, infile) == 7)
+    {
+        // Replace '\n' with '\0'
+        buffer[6] = '\0';
+
+        // Save plate number in array
+        plates[idx] = buffer;
+        idx++;
+    }
+    fclose(infile);
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%s\n", plates[i]);
+    }
+}
