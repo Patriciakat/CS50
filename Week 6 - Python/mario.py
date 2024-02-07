ad25e67ce8d070074de9edc571972db480a1b391
Patricija Katinaite <patricija.katinaite@gmail.com>
2024-02-08 01:10:53 +0200

    Week 6

---
+++ Week 6 - Python/mario.py
@@ -0,0 +1,19 @@
+﻿from cs50 import get_int
+
+while True:
+    height = get_int("Enter pyramid's height: ")
+    if height > 8:
+        print("Enter number smaller than 8")
+    elif height < 1:
+        print("Enter positive number")
+    else: break
+
+for i in range(height):
+    for j in range(height - i):
+        print(" ", end="")
+    for j in range(i+1):
+        print("#", end="")
+    print("  ", end="")
+    for j in range(i+1):
+        print("#", end="")
+    print()
